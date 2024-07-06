2c6ce732d989e468d820f16ed56887c46dbde51f
JATIN2003 <125500755+jatshyr17@users.noreply.github.com>
2024-07-06 22:35:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './index.css'
-import { BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { BrowserRouter, RouterProvider} from 'react-router-dom'
 import PlayercontextProvider from './context/Playercontext.jsx'
 
 

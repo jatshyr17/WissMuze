b2c03492ffaee05b690f8393d5da7c24961ba5f1
JATIN2003 <125500755+jatshyr17@users.noreply.github.com>
2024-07-06 22:35:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Auth0Provider } from '@auth0/auth0-react'
+
 import './index.css'
 import { BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom'
 import PlayercontextProvider from './context/Playercontext.jsx'
@@ -10,12 +10,7 @@ import PlayercontextProvider from './context/Playercontext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain='dev-uwmuldkxvnl4yyfg.us.auth0.com'
-      clientId='VfQGeuzqosDKlOzCTQj0XOmDHURUKXP2'
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}>
+   
     <BrowserRouter>
     <PlayercontextProvider>
     
@@ -25,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </PlayercontextProvider>
     
      </BrowserRouter>
-     </Auth0Provider>
+    
     </React.StrictMode>
     
 )

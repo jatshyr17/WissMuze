b16aff9897105e00b67821b3c2bbd07dcaa4572d
JATIN2003 <125500755+jatshyr17@users.noreply.github.com>
2024-07-31 21:25:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useState,useEffect } from 'react'
+mport React, { useContext,useState,useEffect } from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import { assets } from './assets/assets'
@@ -8,49 +8,32 @@ import { Playercontext } from './context/Playercontext'
 
 
 function App() {
-  const{audioRef,track}=useContext(Playercontext);
-  const [isLoading, setIsLoading] = useState(true);
+  const{audioRef,track}=useContext(Playercontext);
+  return (
+    <>
+    <div className='bg-blue-950 w-screen h-screen sm:[640px] md:[768px]  '>
+      <div className='h-[90%] flex '>
+       
+      <Sidebar/>
+      <Display/>
+       
+       
+       
+      </div>
+     
+      <div className='mt-2 '>
+    <Player/>
+    <audio ref={audioRef} src={track.file}preload='auto'></audio>
+    </div>
+   
 
-  useEffect(() => {
-    // Simulate data fetching
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000); // Replace 2000 with actual loading time
-  }, []);
-
-  return (
-    
-    <div className='bg-blue-950 flex justify-center items-center h-screen'>
-    {isLoading ? (
-        <div className="text-center">
-          <img src={assets.spotify_logo} alt="Loading..." className="max-w-[100px]" />
-        </div>
-      ) : (
-      <>
-    <div className='bg-blue-950 w-screen h-screen sm:[640px] md:[768px]   '>
-      <div className='h-[90%] flex '>
-       
-      <Sidebar/>
-      <Display/>
-        
-        
-        
-      </div>
-      
-      <div className='mt-2 '>
-    <Player/>
-    <audio ref={audioRef} src={track.file}preload='auto'></audio>
-    </div>
-    
-
-    </div>
-   
-   
-     
-    </>
-      )}
-    </div>
-  );
+    </div>
+   
+   
+     
+    </>
+   
+  )
 }
 
 export default App
